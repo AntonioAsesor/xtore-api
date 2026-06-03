c57b8d0170fbda8e243280484e9f0783342e609b
AntonioAsesor <123031166+AntonioAsesor@users.noreply.github.com>
2026-06-03 16:21:38 -0600

    Add header for JSON response in index.php

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
+header('Content-Type: application/json');
 include 'config.php';
+
 echo json_encode([
     "mensaje" => "Hola, soy la API de Xtore y estoy conectada a Railway",
     "estado" => "operativo"
